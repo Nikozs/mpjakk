62ba194903957bd69d654c287ff6aa3a1f066023
Nikozs <45226198+Nikozs@users.noreply.github.com>
2021-03-30 18:42:08 +0300

    Fetching data with AJAX, Task A

--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -1,21 +1,32 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/forbid-prop-types */
 
-import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 import MediaRow from './MediaRow';
 
-const MediaTable = ({ picArray }) => (
-  <table>
-    <tbody>
-      {picArray.map((item, index) => (
-        <MediaRow key={index} file={item} />
-      ))}
-    </tbody>
-  </table>
-);
+const MediaTable = () => {
+  const [picArray, setPicArray] = useState([]);
 
-MediaTable.propTypes = {
-  picArray: PropTypes.array.isRequired,
+  useEffect(() => {
+    const loadMedia = async () => {
+      const response = await fetch('test.json');
+      const json = await response.json();
+      console.log(json);
+      setPicArray(json);
+    };
+
+    loadMedia();
+  }, []);
+
+  return (
+    <table>
+      <tbody>
+        {picArray.map((item, index) => (
+          <MediaRow key={index} file={item} />
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default MediaTable;
